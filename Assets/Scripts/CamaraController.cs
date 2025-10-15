0ba2295e77f6b53d026be461cd0c65097e708e24
Santy834 <santiago21.benitez@gmail.com>
2025-10-15 16:30:05 -0300

    Version lista para demo 1

--- Assets/Scripts/CamaraController.cs
+++ Assets/Scripts/CamaraController.cs
@@ -8,6 +8,7 @@ public class CamaraController : MonoBehaviour
     
     void LateUpdate()
     {
+        if (player == null) return; // verificamos que el jugador no sea nulo
         Vector3 desiredPosition = player.position + offset; // posición deseada de la cámara
         Vector3 smoothedPosition = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed); // suavizamos el movimiento
         transform.position = smoothedPosition; // actualizamos la posición de la cámara
