9bb5b4885561a9a36890ed7f7cce212a44813ff4
Santy834 <santiago21.benitez@gmail.com>
2025-10-24 00:28:16 -0300

    Otras mejoras

--- Assets/Scripts/HabilidadMoverObjetos2.cs
+++ Assets/Scripts/HabilidadMoverObjetos2.cs
@@ -9,17 +9,75 @@ public class HabilidadMoverObjeto : MonoBehaviour
     private float tiempoArrastre = 0f;
     private float tiempoMaximoArrastre = 1f;
     private float alturaFija = 1f;
-    private 
+
+
+    public LayerMask DisUI;
+    public Texture2D manito;
+    public Material normal;
+    public Material brillo;
+
+    private GameObject ultimoObjeto;
+
 
     void Start()
     {
         cam = Camera.main;
     }
-   
+
+    void CambiarCursor()
+    {
+
+        Ray ray = Camera.main.ScreenPointToRay(Mouse.current.position.ReadValue());
+        if (Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, DisUI))
+        {
+            GameObject objetoactual = hit.collider.gameObject;
+            Cursor.SetCursor(manito, Vector2.zero, CursorMode.Auto);
+
+            //Apagamos el brillo del ultimo objeto si es diferente al actual
+            if (objetoactual != ultimoObjeto)
+            {
+                if (ultimoObjeto != null)
+                {
+                    Renderer rendUltimo = ultimoObjeto.GetComponent<Renderer>(); // referencia al renderer del último objeto
+                    if (rendUltimo != null)
+                    {
+                        rendUltimo.material = normal; //Apaga el brillo
+                    }
+                }
+            }
+            //Aplicamos brillo al objeto actual
+            Renderer RendActual = objetoactual.GetComponent<Renderer>();
+            if (RendActual != null)
+            {
+                RendActual.material = brillo; //Aplica el brillo
+            }
+            ultimoObjeto = objetoactual; //Actualizamos el ultimo objeto
+        }
+        else
+        {
+
+            Cursor.SetCursor(null, Vector2.zero, CursorMode.Auto);
+
+            if (ultimoObjeto != null)
+            {
+                Renderer rendUltimo = ultimoObjeto.GetComponent<Renderer>(); // referencia al renderer del último objeto
+                if (rendUltimo != null)
+                {
+                    rendUltimo.material = normal; //Apaga el brillo
+                }
+            }
+
+        }
+
+
+    }
+
     void Update()
     {
-            // Selección con click derecho
-            if (Mouse.current.rightButton.wasPressedThisFrame)
+        CambiarCursor();
+
+        // Selección con click derecho
+        if (Mouse.current.rightButton.wasPressedThisFrame)
             {
                 Ray ray = cam.ScreenPointToRay(Mouse.current.position.ReadValue());
                 if (Physics.Raycast(ray, out RaycastHit hit))
