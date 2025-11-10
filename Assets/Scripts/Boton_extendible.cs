e527dd3d66bb7d89fc95f70ba61e1f8af926df4b
TotoRojo99 <alfonsotobiasboca@gmail.com>
2025-11-10 15:31:53 -0300

    videos_menu_elegir_arma

--- Assets/Scripts/Boton_extendible.cs
+++ Assets/Scripts/Boton_extendible.cs
@@ -1,41 +1,58 @@
 using UnityEngine;
 using UnityEngine.EventSystems;
+using UnityEngine.Video;
 
 public class ButtonHoverTutorial : MonoBehaviour, IPointerEnterHandler, IPointerExitHandler
 {
-    
-    public GameObject tutorialText;
+    [Header("Recuadro del video")]
+    public GameObject recuadroVideo; // El Raw Image con el VideoPlayer
     public GameObject fondo;
-    public float scaleFactor = 1.2f;
-    public float moveUpAmount = 20f;
-    public float animationSpeed = 10f;
 
-    private Vector3 originalScale;
-    private Vector2 originalPos;
+    [Header("Video de la habilidad")]
+    public VideoClip videoHabilidad;
+
+    private VideoPlayer videoPlayer;
     private bool isHovered;
 
     void Start()
     {
-        if (tutorialText != null)
-            tutorialText.SetActive(false);
+        if (recuadroVideo != null)
+        {
+            videoPlayer = recuadroVideo.GetComponent<VideoPlayer>();
+            recuadroVideo.SetActive(false);
+        }
+
+        if (fondo != null)
             fondo.SetActive(false);
     }
-
-   
 
     public void OnPointerEnter(PointerEventData eventData)
     {
         isHovered = true;
-        if (tutorialText != null)
-            tutorialText.SetActive(true);
+
+        if (recuadroVideo != null)
+        {
+            recuadroVideo.SetActive(true);
+            videoPlayer.clip = videoHabilidad;
+            videoPlayer.isLooping = true;
+            videoPlayer.Play();
+        }
+
+        if (fondo != null)
             fondo.SetActive(true);
     }
 
     public void OnPointerExit(PointerEventData eventData)
     {
         isHovered = false;
-        if (tutorialText != null)
-            tutorialText.SetActive(false);
+
+        if (recuadroVideo != null)
+        {
+            videoPlayer.Stop();
+            recuadroVideo.SetActive(false);
+        }
+
+        if (fondo != null)
             fondo.SetActive(false);
     }
 }
