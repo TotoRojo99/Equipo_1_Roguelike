d8b53e2940ccc2ec9234e10be94ab739c8b9944d
Santy834 <santiago21.benitez@gmail.com>
2025-11-11 19:56:01 -0300

    Correciones

--- Assets/Scripts/HabilidadPlayer.cs
+++ Assets/Scripts/HabilidadPlayer.cs
@@ -11,6 +11,7 @@ public class HabilidadPlayer : MonoBehaviour
     public GameObject Derr_objeto2;
     public GameObject Derr_objeto3;*/
     public GameObject Rayo;
+    public GameObject CambioSkin;
     public GameObject Agujero;
     public HabilidadMoverObjeto hab_MovObj;
     //public HabilidadRetrocederposicion hab_RetroPos;
@@ -21,6 +22,7 @@ public class HabilidadPlayer : MonoBehaviour
     private Derrumbe_objeto der_obj1;
     private Derrumbe_objeto der_obj2;
     private Derrumbe_objeto der_obj3;*/
+    private Cambio_Skin CSkin;
     private RayoLaser RLaser;
     private HabilidadAgujeroNegro HAgujero;
 
@@ -30,6 +32,7 @@ public class HabilidadPlayer : MonoBehaviour
         RLaser = Rayo.GetComponent<RayoLaser>();
         HAgujero = Agujero.GetComponent<HabilidadAgujeroNegro>();
         tb = TBala.GetComponent<BulletTimeController>();
+        CSkin = CambioSkin.GetComponent<Cambio_Skin>();
         //der_obj = Derr_objeto.GetComponent<Derrumbe_objeto>();
         /*der_obj1 = Derr_objeto1.GetComponent<Derrumbe_objeto>();
         der_obj2 = Derr_objeto2.GetComponent<Derrumbe_objeto>();
@@ -46,6 +49,7 @@ public class HabilidadPlayer : MonoBehaviour
             hab_MovObj.enabled = true;
 
             RLaser.enabled = false;
+            CSkin.enabled = false;
             HAgujero.enabled = false;
             /*der_obj.enabled = false;
             der_obj1.enabled = false;
@@ -62,6 +66,7 @@ public class HabilidadPlayer : MonoBehaviour
             hab_MovObj.enabled = false;
 
             RLaser.enabled = true;
+            CSkin.enabled = true;
             HAgujero.enabled = true;
             /*der_obj.enabled = true;
             der_obj1.enabled = true;
